98a7c3318001b932fb4ba57776b7431e13e0f3d6
ionelac <fcomsa40@gmail.com>
2022-10-18 15:22:40 +0300

    Add project files.

---
+++ Comsa_Ionela_Florentina_Lab2/Data/Comsa_Ionela_Florentina_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Comsa_Ionela_Florentina_Lab2.Models;
+
+namespace Comsa_Ionela_Florentina_Lab2.Data
+{
+    public class Comsa_Ionela_Florentina_Lab2Context : DbContext
+    {
+        public Comsa_Ionela_Florentina_Lab2Context (DbContextOptions<Comsa_Ionela_Florentina_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Comsa_Ionela_Florentina_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Comsa_Ionela_Florentina_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Comsa_Ionela_Florentina_Lab2/Models/Publisher.cs
@@ -0,0 +1,10 @@
+﻿namespace Comsa_Ionela_Florentina_Lab2.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string PublisherName { get; set; }
+        public ICollection<Book>? Books { get; set; } //navigation property
+    }
+
+}
